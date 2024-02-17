92e14654a773229023dc063831c259f8592470dc
Hacker001uzb <156788115+Hacker001uzb@users.noreply.github.com>
2024-02-17 15:27:12 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var elInp1 =document.querySelector('.inp1')
+
+function fn(){
+    var bor =elInp1.value
+    if(bor == bor.trim()){
+        console.log('probel yoge')
+    }
+    else{
+        console.log('probel bore')
+    }
+}
